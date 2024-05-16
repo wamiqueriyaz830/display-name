2d691ead1f60b2a9c170e405c300bb4fc72ca5d3
wamiqueriyaz830 <wamiqueriyaz00@gmail.com>
2024-05-16 22:40:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
   const handleSubmit=(e)=>{
    e.preventDefault()
    setData({fname,lname})
-   setFname("")
-   setLname("")
+   // setFname("")
+   // setLname("")
   }
   return (
    <>
